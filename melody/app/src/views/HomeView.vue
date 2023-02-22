cc5093fbafdf0eb5f261780bd6b32792c49ed29e
Nikita Tikhonov <nekitdevofficial@gmail.com>
2023-02-22 20:13:18 +0300

    Partially implement `nav` sidebar.

--- melody/app/src/views/HomeView.vue
+++ melody/app/src/views/HomeView.vue
@@ -2,8 +2,8 @@
   <header v-if="isAuthorized">
     <Menu as="div" class="relative inline-block text-left">
       <MenuButton class="inline-flex w-full items-center justify-center rounded-full bg-neutral-200 dark:bg-black text-neutral-900 dark:text-neutral-50 hover:bg-neutral-100 dark:hover:bg-neutral-800 focus:outline-none gap-x-2 pr-4">
-        <img class="h-10 w-auto rounded-full" :src="`https://melodykit.app/static/images/user.${user.id}.png`"/>
-        <span>{{ user.name }}</span>
+        <img class="h-10 w-auto rounded-full" :src="`https://melodykit.app/static/images/user.png`"/>
+        <span>nekitdev</span>
       </MenuButton>
       <MenuItems class="absolute py-1 px-1 mt-2 w-52 divide-y divide-neutral-100 dark:divide-neutral-700 rounded-md bg-neutral-200 dark:bg-neutral-800 text-neutral-900 dark:text-neutral-50 focus:outline-none">
         <div>
@@ -21,25 +21,23 @@
         </div>
       </MenuItems>
     </Menu>
-    <!--
-    <nav class="flex min-h-screen">
-      <div class="hidden w-64 bg-neutral-50 dark:bg-black lg:block text-xl">
-        <a class="flex items-center px-6 py-2 gap-x-4 text-melody-blue" href="/">
-          <i class="fa-solid fa-rotate text-3xl"></i>
-          <span>Home</span>
-        </a>
-        <a class="flex items-center px-6 py-2 gap-x-4" href="/search">
-          <i class="fa-solid fa-magnifying-glass text-3xl"></i>
-          <span>Search</span>
-        </a>
-        <a class="flex items-center px-6 py-2 gap-x-4" href="/library">
-          <i class="fa-solid fa-book text-3xl"></i>
-          <span>Library</span>
-        </a>
-      </div>
-    </nav>
-    -->
   </header>
+  <nav class="flex min-h-screen">
+    <div class="hidden w-64 bg-neutral-50 dark:bg-black lg:block">
+      <a class="flex items-center px-6 py-2 gap-x-4 text-melody-blue" href="/">
+        <i class="fa-solid fa-rotate w-5 h-auto"></i>
+        <span class="text-xl">Home</span>
+      </a>
+      <a class="flex items-center px-6 py-2 gap-x-4" href="/search">
+        <i class="fa-solid fa-magnifying-glass w-5 h-auto"></i>
+        <span class="text-xl">Search</span>
+      </a>
+      <a class="flex items-center px-6 py-2 gap-x-4" href="/library">
+        <i class="fa-solid fa-book w-5 h-auto"></i>
+        <span class="text-xl">Library</span>
+      </a>
+    </div>
+  </nav>
 </template>
 
 <script>
