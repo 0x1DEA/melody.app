e3709a92375bbe765fb3eac60aa004c1d18aec02
Nikita Tikhonov <nekitdevofficial@gmail.com>
2023-02-23 04:31:48 +0300

    Add `tauri`.

--- melody/app/src/views/HomeView.vue
+++ melody/app/src/views/HomeView.vue
@@ -1,5 +1,5 @@
-<template>
-  <header v-if="isAuthorized" class="flex justify-between">
+<template v-if="isAutorized">
+  <header class="flex justify-between">
     <nav class="flex min-h-screen">
       <div class="hidden w-64 bg-neutral-50 dark:bg-black lg:block px-4 py-2">
         <div class="divide-y divide-neutral-200 dark:divide-neutral-700">
@@ -40,28 +40,43 @@
         </div>
       </div>
     </nav>
-    <Menu as="div" class="relative inline-block text-left mt-4 mr-4">
-      <MenuButton class="inline-flex w-full items-center justify-center rounded-full bg-neutral-200 dark:bg-black text-neutral-900 dark:text-neutral-50 hover:bg-neutral-100 dark:hover:bg-neutral-800 focus:outline-none gap-x-2 pr-4">
-        <img class="h-10 w-auto rounded-full" :src="`https://melodykit.app/static/images/user.${user.id}.png`"/>
-        <span>{{ user.name }}</span>
-      </MenuButton>
-      <MenuItems class="absolute py-1 px-1 mt-2 w-52 divide-y divide-neutral-100 dark:divide-neutral-700 rounded-md bg-neutral-200 dark:bg-neutral-800 text-neutral-900 dark:text-neutral-50 focus:outline-none right-0">
-        <div>
-          <MenuItem v-slot="{ active }">
-            <a href="/me" :class="[active ? 'bg-neutral-100 dark:bg-neutral-700' : '', 'block rounded-sm px-4 py-2']">Profile</a>
-          </MenuItem>
-          <MenuItem v-slot="{ active }">
-            <a href="/settings" :class="[active ? 'bg-neutral-100 dark:bg-neutral-700' : '', 'block rounded-sm px-4 py-2']">Settings</a>
-          </MenuItem>
+    <div v-if="isApp">
+      <div class="flex items-center sticky">
+        <div @click="control(MINIMIZE)" class="px-4 py-1 bg-white dark:bg-black hover:bg-neutral-200 dark:hover:bg-neutral-800">
+          <i class="fa-solid fa-minus"></i>
         </div>
-        <div>
-          <MenuItem v-slot="{ active }">
-            <a href="https://melodykit.app/logout" :class="[active ? 'bg-neutral-100 dark:bg-neutral-700' : '', 'block rounded-sm px-4 py-2']">Log out</a>
-          </MenuItem>
+        <div @click="control(RESTORE)" class="px-4 py-1 bg-white dark:bg-black hover:bg-neutral-200 dark:hover:bg-neutral-800">
+          <i class="fa-solid fa-window-restore"></i>
         </div>
-      </MenuItems>
-    </Menu>
+        <div @click="control(CLOSE)" class="px-4 py-1 bg-white dark:bg-black hover:bg-red-600">
+          <i class="fa-solid fa-xmark"></i>
+        </div>
+      </div>
+    </div>
   </header>
+  <!--
+  <Menu as="div" class="absolute inline-block text-left mt-4 mr-4">
+    <MenuButton class="inline-flex w-full items-center justify-center rounded-full bg-neutral-200 dark:bg-black text-neutral-900 dark:text-neutral-50 hover:bg-neutral-100 dark:hover:bg-neutral-800 focus:outline-none gap-x-2 pr-4">
+      <img class="h-10 w-auto rounded-full" :src="`https://melodykit.app/static/images/user.png`"/>
+      <span>name</span>
+    </MenuButton>
+    <MenuItems class="absolute py-1 px-1 mt-2 w-52 divide-y divide-neutral-100 dark:divide-neutral-700 rounded-md bg-neutral-200 dark:bg-neutral-800 text-neutral-900 dark:text-neutral-50 focus:outline-none right-0">
+      <div>
+        <MenuItem v-slot="{ active }">
+          <a href="/me" :class="[active ? 'bg-neutral-100 dark:bg-neutral-700' : '', 'block rounded-sm px-4 py-2']">Profile</a>
+        </MenuItem>
+        <MenuItem v-slot="{ active }">
+          <a href="/settings" :class="[active ? 'bg-neutral-100 dark:bg-neutral-700' : '', 'block rounded-sm px-4 py-2']">Settings</a>
+        </MenuItem>
+      </div>
+      <div>
+        <MenuItem v-slot="{ active }">
+          <a href="https://melodykit.app/logout" :class="[active ? 'bg-neutral-100 dark:bg-neutral-700' : '', 'block rounded-sm px-4 py-2']">Log out</a>
+        </MenuItem>
+      </div>
+    </MenuItems>
+  </Menu>
+  -->
 </template>
 
 <script>
@@ -92,11 +107,36 @@ export default defineComponent({
     },
     userPlaylists() {
       return this.$store.getters.stateUserPlaylists;
+    },
+    isApp() {
+      return Boolean(window.__TAURI_METADATA__);
     }
   }
 });
 </script>
 
 <script setup>
-import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/vue";
+// import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/vue";
+
+import {appWindow} from "@tauri-apps/api/window";
+
+const MINIMIZE = 0;
+const RESTORE = 1;
+const CLOSE = 2;
+
+async function control(mode) {
+  switch (mode) {
+    case MINIMIZE:
+      await appWindow.minimize();
+      break;
+
+    case RESTORE:
+      await appWindow.toggleMaximize();
+      break;
+
+    case CLOSE:
+      await appWindow.close();
+      break;
+  }
+}
 </script>
